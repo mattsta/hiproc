877b5948444344d3d361e7fc205c9435bdcb8d6d
Matt Stancliff <matt@genges.com>
2025-09-25 20:50:29 -0400

    Disable uvicorn dev/reload mode by default
    Easily re-enabled with a command line option for more interactive
    development etc.

--- src/hiproc/cli.py
+++ src/hiproc/cli.py
@@ -12,9 +12,15 @@ def main():
         "--host", type=str, default="127.0.0.1", help="The host to bind to."
     )
     parser.add_argument("--port", type=int, default=8128, help="The port to run on.")
+    parser.add_argument(
+        "--dev",
+        "--reload",
+        action="store_true",
+        help="Enable development mode with auto-reload (higher CPU usage).",
+    )
     args = parser.parse_args()
 
-    uvicorn.run("hiproc.main:app", host=args.host, port=args.port, reload=True)
+    uvicorn.run("hiproc.main:app", host=args.host, port=args.port, reload=args.dev)
 
 
 if __name__ == "__main__":
